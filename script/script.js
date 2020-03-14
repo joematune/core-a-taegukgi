c8c036a361d3fb7b6d3ace468730cb94eb61bfa1
joematune <joematune@gmail.com>
2020-03-14 16:58:19 +0200

    Simplify into a single timeline

--- script/script.js
+++ script/script.js
@@ -1,60 +1,72 @@
-let paused = false;
-let masterTimeline = paused ? gsap.timeline({ paused: true }) : gsap.timeline({delay: 1});
-gsap.globalTimeline.timeScale(1.5);
+let tl = gsap.timeline({ defaults: { duration: 1, transformOrigin: '50% 50%' }});
+tl.addLabel('start', 0);
+tl.timeScale(1);
+tl.repeat(-1);
 
-gsap.set('.yingyang', { transformOrigin: '50% 50%', rotate: 34 });
-
-let yingAndYangTimeline = () => {
-    let tl = gsap.timeline({ defaults: { duration: 1.8 } });
-    tl.add('start');
-    tl.to('.yang', {
-        fillOpacity: '100%',
-        morphSVG: '.core-right'
-    }, 'start');
-    tl.to('.ying', {
-        morphSVG: '.core-left'
-    }, 'start');
-    tl.to('.middle-a-bits', {
-        duration: 1,
-        fillOpacity: '1'
-    }, '-=0.7');
-    return tl;
-}
-masterTimeline.add(yingAndYangTimeline);
-let yingYangCircleTimeline = () => {
-    let tl = gsap.timeline({ defaults: { duration: 2 } });
-    tl.to('.yingyang', {
-        transformOrigin: '50% 50%',
-        rotate: -720
-    });
-    return tl;
-}
-masterTimeline.add(yingYangCircleTimeline);
-
-let coreSidesTimeline = () => {
-    let tl = gsap.timeline({ defaults: { duration: 1} });
-    tl.add('start');
-    tl.to('.core-right', {
-        morphSVG: '.ying'
-    }, 'start');
-    tl.to('.core-left', {
-        fillOpacity: '50%',
-        morphSVG: '.yang'
-    }, 'start');
-    tl.to('.middle-of-a', {
-        duration: 0.5,
-        fillOpacity: '0'
-    }, 'start');
-    return tl;
-}
-masterTimeline.add(coreSidesTimeline);
-
-let coreACircleTimeline = () => {
-    let tl = gsap.timeline({ defaults: { duration: 2} });
-    tl.to('.core-a', {
-        transformOrigin: '50% 50%',
+tl.to('.core-a', {
+        duration: 4,
         rotate: -696
-    });
-    return tl;
-}
-masterTimeline.add(coreACircleTimeline);
+      }, 'start')
+
+      .addLabel('fullSpin', '>')
+
+      .to('.middle-of-a', {
+        ease: 'none',
+        scaleX: 0,
+        scaleY: 0.9,
+        transformOrigin: '50% 0%'
+      }, 'start')
+      .to('.core-right', {
+        ease: 'none',
+        morphSVG: '.core-right-big'
+      }, 'start')
+      .to('.core-left', {
+        ease: 'none',
+        morphSVG: '.core-left-big',
+        fillOpacity: 0.5
+      }, 'start')
+
+      .addLabel('circleClosed', '>')
+
+      .to('.core-right', {
+        ease: 'none',
+        morphSVG: '.ying'
+      }, 'circleClosed')
+      .to('.core-left', {
+        ease: 'none',
+        morphSVG: '.yang'
+      }, 'circleClosed')
+      .to('.core-right', {
+        duration: 0
+      }, 'circleClosed')
+
+      .to('.core-a', {
+        duration: 4,
+        rotate: -1440
+      }, 'fullSpin')
+
+      .addLabel('secondSpin', '<')
+
+      .to('.core-right', {
+        ease: 'none',
+        morphSVG: '.core-right-big'
+      }, 'secondSpin')
+      .to('.core-right', {
+        morphSVG: '.core-right'
+      }, '>')
+      .to('.core-left', {
+        ease: 'none',
+        morphSVG: '.core-left-big'
+      }, 'secondSpin')
+      .to('.core-left', {
+        morphSVG: '.core-left',
+        fillOpacity: 1
+      }, '>')
+
+      .addLabel('circleOpen', '<')
+
+      .to('.middle-of-a', {
+        scaleX: 1,
+        scaleY: 1,
+        transformOrigin: '50% 0%'
+      }, 'circleOpen')
